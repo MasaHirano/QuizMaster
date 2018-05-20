5117a9cfca2771139ffa02d847953ecef08deb3b
Masataka Hirano <gooogle4747@gmail.com>
2018-05-20 15:55:27 +0900

    Update App.js. show questions

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+    this.getQuestions = this.getQuestions.bind(this)
+    this.getQuestion = this.getQuestion.bind(this)
+  }
+
+  componentDidMount() {
+    this.getQuestions()
+  }
+
+  fetch(endpoint) {
+    return window.fetch(endpoint)
+      .then(response => response.json())
+      .catch(error => console.log(error))
+  }
+
+  getQuestions() {
+    this.fetch('/api/questions')
+      .then(questions => {
+        if (questions.length) {
+          this.setState({ questions: questions })
+          this.getQuestion(questions[0].id)
+        } else {
+          this.setState({ questions: [] })
+        }
+      })
+  }
+
+  getQuestion(id) {
+    this.fetch(`/api/questions/${id}`)
+      .then(question => this.setState({question: question}))
+  }
+
   render() {
+    let { questions, question } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Container text>
+        <Header as='h2' icon textAlign='center' color='teal'>
+          <Icon name='unordered list' circular />
+          <Header.Content>
+            Questions
+          </Header.Content>
+        </Header>
+        <Divider hidden section />
+      {questions &&
+        <Container>
+          <Segment.Group>
+            {questions.map((question, i) => {
+              return (
+                <Segment key={i} onClick={() => this.getQuestion(questions[i].id)}>
+                  <Link to='/about'>{question.content}</Link>
+                </Segment>
+              )
+            })}
+          </Segment.Group>
+        </Container>
+      }
+      </Container>
+    )
   }
 }
 
-export default App;
+export default App
