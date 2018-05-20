e96f438337a8e417bac31f63a8ab7cf85213eadb
Masataka Hirano <gooogle4747@gmail.com>
2018-05-20 13:35:50 +0900

    Remove some actions from QuestionsController because api accepts only GET requests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
-class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show, :update, :destroy]
+class QuestionsController < ApiController
+  before_action :set_question, only: [:show]
 
   # GET /questions
   def index
@@ -13,39 +13,9 @@ class QuestionsController < ApplicationController
     render json: @question
   end
 
-  # POST /questions
-  def create
-    @question = Question.new(question_params)
-
-    if @question.save
-      render json: @question, status: :created, location: @question
-    else
-      render json: @question.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /questions/1
-  def update
-    if @question.update(question_params)
-      render json: @question
-    else
-      render json: @question.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /questions/1
-  def destroy
-    @question.destroy
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_question
       @question = Question.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def question_params
-      params.require(:question).permit(:content, :answer)
     end
 end
