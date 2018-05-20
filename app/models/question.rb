6826999cfe3b3a5441e1aae1ad5ebc0b24c05eb0
Masataka Hirano <gooogle4747@gmail.com>
2018-05-20 13:56:54 +0900

    Add annotations to Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :bigint(8)        not null, primary key
+#  content    :string
+#  answer     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Question < ApplicationRecord
 end
