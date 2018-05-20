7aa1a119bba2808f6667dec552455ce2f5a4dd38
Masataka Hirano <gooogle4747@gmail.com>
2018-05-20 23:37:47 +0900

    Update question.rb to validate html tags and attributes

--- app/models/question.rb
+++ app/models/question.rb
@@ -12,6 +12,7 @@
 class Question < ApplicationRecord
 
   validates :content, :answer, presence: true
+  validate :content_can_only_accept_whitelisted_tags_or_attributes
 
   def check_answer(given_answer)
     correct_answer, given_answer = [answer, given_answer].map { |str| str.strip.downcase }
@@ -27,5 +28,14 @@ class Question < ApplicationRecord
 
   def numbers_to_words(string)
     string.gsub(/\d+/) { |str| str.to_i.to_words }
+  end
+
+  def content_can_only_accept_whitelisted_tags_or_attributes
+    return if content.blank?
+    sanitized = ActionController::Base.helpers.sanitize(content, attributes: %w(style))
+    if content != sanitized
+      errors.add(:content,
+        "Disallowed tags or attributes are contained. It should be `#{ERB::Util.html_escape(sanitized.to_str)}`")
+    end
   end
 end
