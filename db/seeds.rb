9f21c465d4a3b28a7e0f1a7daa03fee1698dea1c
Masataka Hirano <gooogle4747@gmail.com>
2018-05-20 13:31:02 +0900

    Add ActiveAdmin resources for Question model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+now = DateTime.now
+question_keys = %i(content answer created_at updated_at)
+question_values = [
+  ['How many letters are there in the English alphabet?', '26', now, now],
+  ['What is the capital of Japan?', 'Tokyo', now, now],
+]
+Question.create!(question_values.map { |value| question_keys.zip(value).to_h })
