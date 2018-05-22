6c736cc09e454f7370d6a24858795c999844f0c6
Masataka Hirano <gooogle4747@gmail.com>
2018-05-23 00:04:34 +0900

    Update questions_routing_spec to actual paths

--- spec/routing/questions_routing_spec.rb
+++ spec/routing/questions_routing_spec.rb
@@ -4,29 +4,15 @@ RSpec.describe QuestionsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/questions").to route_to("questions#index")
+      expect(:get => "/api/questions").to route_to("questions#index")
     end
-
 
     it "routes to #show" do
-      expect(:get => "/questions/1").to route_to("questions#show", :id => "1")
+      expect(:get => "/api/questions/1").to route_to("questions#show", :id => "1")
     end
 
-
-    it "routes to #create" do
-      expect(:post => "/questions").to route_to("questions#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/questions/1").to route_to("questions#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/questions/1").to route_to("questions#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/questions/1").to route_to("questions#destroy", :id => "1")
+    it "routes to #answer" do
+      expect(:post => "/api/questions/1/answer").to route_to("questions#answer", :id => "1")
     end
 
   end
