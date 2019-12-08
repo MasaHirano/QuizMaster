bd3295762130da4fd02db89acd12f019e214df74
alpaca0984 <gooogle4747@gmail.com>
2019-12-08 20:23:55 +0900

    Remove redux-actions from Home module

--- client/src/reducers/homeReducer.js
+++ client/src/reducers/homeReducer.js
@@ -1,15 +1,18 @@
-import { handleActions } from 'redux-actions'
-
 import {
-  receiveQuestions
+  RECEIVE_QUESTIONS
 } from '../actions/homeActions'
 
 const initialState = {
   questions: [{ id: NaN, content: '' }]
 }
 
-export default handleActions({
-  [receiveQuestions]: (state, action) => ({
-    ...state, ...action.payload
-  })
-}, initialState)
+export default function homeReducer(state = initialState, action) {
+  switch (action.type) {
+    case RECEIVE_QUESTIONS:
+      return Object.assign({}, state, {
+        questions: action.questions
+      })
+    default:
+      return state
+  }
+}
