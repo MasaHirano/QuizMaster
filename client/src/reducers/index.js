b40b6109d4687436b2393353a16908cf36229479
alpaca0984 <gooogle4747@gmail.com>
2019-12-08 22:13:25 +0900

    Upgrade all packages

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,2 +1,12 @@
-export { default as homeReducer } from './homeReducer'
-export { default as questionReducer } from './questionReducer'
+import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
+
+import homeReducer from './homeReducer'
+import questionReducer from './questionReducer'
+
+const createRootReducer = (history) => combineReducers({
+  router: connectRouter(history),
+  homeReducer,
+  questionReducer
+})
+export default createRootReducer
