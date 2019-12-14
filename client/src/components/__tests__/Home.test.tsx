2e1ea19c8a51fee7c2be578b52750c2580775604
alpaca0984 <gooogle4747@gmail.com>
2019-12-14 18:54:53 +0900

    Migrate tests to TypeScript except Question module

---
+++ client/src/components/__tests__/Home.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react'
+import { shallow, ShallowWrapper } from 'enzyme'
+
+import Home from '../Home'
+
+describe('<Home />', () => {
+  let onDidMountMock: jest.Mock
+  let wrapper: ShallowWrapper
+
+  beforeAll(() => {
+    const questions = [
+      { id: 1, content: 'foo' },
+      { id: 2, content: 'bar' }
+    ]
+    onDidMountMock = jest.fn()
+    wrapper = shallow(
+      <Home
+        questions={questions}
+        onDidMount={onDidMountMock}
+      />
+    )
+  })
+
+  describe('render()', () => {
+    it('calls onDidMount', () => {
+      expect(onDidMountMock)
+        .toHaveBeenCalled()
+    })
+
+    it('shows two rows when two questions are in state', () => {
+      expect(wrapper.find(Segment))
+        .toHaveLength(2)
+    })
+  })
+})
